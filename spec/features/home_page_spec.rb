fe131bcf13c1ccdb041aff69db7fac9e7b6ffdb7
TJ <77421405+TJ-Blinn@users.noreply.github.com>
2022-04-29 18:04:22 -0400

    [update] add setup phase for 10 fake products

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,19 +4,35 @@ require 'rails_helper'
 # REMINDERS: 
     # feature block => describe block
     # scenario block => it block
-
-
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-  scenario "They see all products" do
     
-    # ACT
-    visit '/'
+    
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+      
+  # setup phase that adds 10 products using the RSpec before :each block:
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
 
-    # DEBUG / VERIFY
-    save_screenshot "test.png"
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
   end
 
+  scenario "They see all products" do    
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+    save_screenshot
+
+    # VERIFY
+    expect(page).to have_css 'article.product'
+  end
 end
 
 # run test => bin/rspec spec/features
